96f49f4dbc2ec97f18d3f4adcd3359b42cff7d18
Falachi <awesomezap4@gmail.com>
2025-03-06 16:07:20 +0800

    add email and password validator. add terms and condition page

---
+++ lib/page/Homeowner/listing/contractor_profile.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:quantocube/page/auth/signup/Contractor/contractor_data.dart';
+
+class ContractorProfile extends StatelessWidget {
+  const ContractorProfile({
+    super.key,
+    required this.contractor,
+  });
+
+  final ContractorData contractor;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/page/auth/login_page.dart
+++ lib/page/auth/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:quantocube/components/button.dart';
+import 'package:quantocube/components/text_field.dart';
 import 'package:quantocube/page/auth/signup/signup_page.dart';
 
 class LoginPage extends StatelessWidget {
@@ -131,7 +133,9 @@ class _LoginBoxContentState extends State<LoginBoxContent> {
           ),
         ),
         const SizedBox(height: 20),
-        LoginButton(
+        LargeOrangeButton.onlyText(
+          context,
+          text: 'Log In',
           onPressed: () =>
               onLogin(emailController.text, passwordController.text),
         ),
@@ -139,96 +143,6 @@ class _LoginBoxContentState extends State<LoginBoxContent> {
         AdditionalButtons(
             onForgetPassword: onForgetPassword, onSignUp: onSignUp)
       ],
-    );
-  }
-}
-
-class TextInputBox extends StatelessWidget {
-  const TextInputBox({
-    super.key,
-    required this.controller,
-    required this.hintText,
-    this.obscureText = false, // default to false
-    this.textInputAction = TextInputAction.next, // default to next
-    this.suffixIcon,
-  });
-
-  final TextEditingController controller;
-  final String hintText;
-  final bool obscureText;
-  final TextInputAction textInputAction;
-  final Widget? suffixIcon;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 70,
-      child: TextFormField(
-        textInputAction: textInputAction,
-        controller: controller,
-        decoration: InputDecoration(
-          //contentPadding: const EdgeInsets.all(20),
-          isDense: false,
-          hintText: hintText,
-          hintStyle: const TextStyle(
-            color: Colors.white,
-          ),
-          filled: true,
-          contentPadding:
-              const EdgeInsets.symmetric(horizontal: 20, vertical: 25),
-          fillColor: Theme.of(context).colorScheme.surface,
-          border: OutlineInputBorder(
-            borderSide: BorderSide.none, // No outline when not focused
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Theme.of(context)
-                  .colorScheme
-                  .primary, // Primary color outline when focused
-              width: 2.0,
-            ),
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          suffixIcon: suffixIcon,
-        ),
-        obscureText: obscureText,
-      ),
-    );
-  }
-}
-
-class LoginButton extends StatelessWidget {
-  const LoginButton({
-    super.key,
-    required this.onPressed,
-  });
-
-  final VoidCallback onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 70,
-      width: double.infinity,
-      child: FilledButton(
-        onPressed: onPressed,
-        style: FilledButton.styleFrom(
-          backgroundColor: Theme.of(context).colorScheme.primary,
-          padding: EdgeInsets.zero,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-        ),
-        child: const Text(
-          'Enter',
-          style: TextStyle(
-            fontWeight: FontWeight.w600,
-            fontSize: 18,
-            color: Colors.white,
-          ),
-        ),
-      ),
     );
   }
 }
